3b4245fb075707cc9214ac9ca686c2b8cdfa7bcf
Eoin Kenny <eoinmkenny@gmail.com>
2017-03-13 10:04:10 +0000

    Changed name of mysql file to 'mysql_db_connection' and added first draft of table 'bike'

--- mysql_db_connection.py
+++ mysql_db_connection.py
@@ -1,4 +1,3 @@
-import pymysql.cursors
 import pymysql
 
 # Connect to the database
@@ -6,11 +5,20 @@ conn = pymysql.connect(host='bikebikebaby2.c1ecxudgvgy2.us-west-2.rds.amazonaws.
                              user='administrator',
                              password='bikebikebaby2',
                              db='bikebikebaby2',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
+                             charset='utf8',)
 
 c = conn.cursor()
 
-'''Insert MySQL Here'''
-
+c.execute("""CREATE TABLE IF NOT EXISTS Bike(
+number INT PRIMARY KEY,
+address TEXT,
+lat INT,
+lng INT,
+status TEXT,
+bike_stands INT,
+available_bike_stands INT,
+available_bikes INT,
+last_update INT
+)""")
+conn.commit()
 conn.close()

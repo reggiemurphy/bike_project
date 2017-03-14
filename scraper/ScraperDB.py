5b2cf782eb4aa878ec94d44a3cebff8ef5855fe3
Rory Harpur <Rory@Rorys-MacBook-Pro.local>
2017-03-14 17:01:58 +0000

    Added temp column to DB.

--- scraper/ScraperDB.py
+++ scraper/ScraperDB.py
@@ -1,63 +1,67 @@
 import pymysql
 
 class ScraperDB:
-	'''Class that deals with creating/connecting/disconnecting/commiting to DB.'''
+    '''Class that deals with creating/connecting/disconnecting/commiting to DB.'''
 
-	def __init__(self):
-		# Opening connection
-		self.open()
-		# Creating cursor.
-		c = self.conn.cursor()
+    def __init__(self):
+        # Opening connection
+        self.open()
+        # Creating cursor.
+        c = self.conn.cursor()
 
-		# Statement to create database if it does not exist. 
-		c.execute("""CREATE TABLE IF NOT EXISTS BikeData(
-		id INT,
-		address TEXT,
-		lat FLOAT(9,6),
-		lng FLOAT(9,6),
-		status TEXT,
-		bike_stands INT,
-		available_bike_stands INT,
-		available_bikes INT,
-		weather TEXT,
-		last_update DATETIME,
-		PRIMARY KEY (id, last_update)
-		)""")
+        # Statement to create database if it does not exist. 
+        c.execute("""CREATE TABLE IF NOT EXISTS BikeData(
+        id INT,
+        address TEXT,
+        lat FLOAT(9,6),
+        lng FLOAT(9,6),
+        status TEXT,
+        bike_stands INT,
+        available_bike_stands INT,
+        available_bikes INT,
+        weather_main TEXT,
+        weather_description TEXT,
+        temp FLOAT(6,3),
+        last_update DATETIME,
+        PRIMARY KEY (id, last_update)
+        )""")
 
-		# Committing statement
-		self.conn.commit()
-		# Closing connection
-		self.conn.close()
-
-
-	#--------------------------------------------------------------------------#
-	# Opening connection to database
-	def open(self):
-		# Connect to the database
-		self.conn = pymysql.connect(host='bikebikebaby2.c1ecxudgvgy2.us-west-2.rds.amazonaws.com',
-			user='administrator',
-			password='bikebikebaby2',
-			db='bikebikebaby2',
-			charset='utf8',)
+        # Committing statement
+        self.conn.commit()
+        # Closing connection
+        self.conn.close()
 
 
-	#--------------------------------------------------------------------------#
-	# Close connection to database
-	def close(self):
-		# Disconnect from database
-		self.conn.close()
-		print("Data Committed, DB Connection Closed") 
-		print("-- Five Minutes Until Next Scrape --\n")
+    #--------------------------------------------------------------------------#
+    # Opening connection to database
+    def open(self):
+        # Connect to the database
+        self.conn = pymysql.connect(host='bikebikebaby2.c1ecxudgvgy2.us-west-2.rds.amazonaws.com',
+            user='administrator',
+            password='bikebikebaby2',
+            db='bikebikebaby2',
+            charset='utf8',)
 
 
-	#--------------------------------------------------------------------------#
-	# Insert row into table
-	def insert(self, number, station, lat, lng, status, total_stands, free_stands, free_bikes, weather, updated):
-		# Creating cursor.
-		c = self.conn.cursor()
+    #--------------------------------------------------------------------------#
+    # Close connection to database
+    def close(self):
+        # Disconnect from database
+        self.conn.close()
+        print("Data Committed, DB Connection Closed") 
+        print("-- Five Minutes Until Next Scrape --\n")
 
-		# Statement to insert row into table. 
-		c.execute("INSERT IGNORE INTO BikeData VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (int(number), station, float(lat), float(lng), status, int(total_stands), int(free_stands), int(free_bikes), weather, str(updated)))
 
-		# Committing statement
-		self.conn.commit()
+    #--------------------------------------------------------------------------#
+    # Insert row into table
+    def insert(self, number, station, lat, lng, status, total_stands, free_stands, free_bikes, weather_main, weather_description, temp, updated):
+        # Creating cursor.
+        c = self.conn.cursor()
+
+        # Statement to insert row into table. 
+        c.execute("INSERT IGNORE INTO BikeData VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", 
+            (int(number), station, float(lat), float(lng), status, int(total_stands), int(free_stands), int(free_bikes), weather_main, weather_description, str(temp), str(updated)))
+
+        # Committing statement
+        self.conn.commit()
+        pass
--- scraper/__main__.py
+++ scraper/__main__.py
@@ -8,102 +8,109 @@ import traceback
 
 #--------------------------------------------------------------------------#
 def main():
-	# Creating instance of ScraperDB to handle database operations. 
-	db = ScraperDB()
+    # Creating instance of ScraperDB to handle database operations. 
+    db = ScraperDB()
 
-	while (True):
-		print(time.strftime('%a, %H:%M:%S'), "- Scraping Data...")
+    while (True):
+        print(time.strftime('%a, %H:%M:%S'), "- Scraping Data...")
 
-		# Getting summary of current weather. 
-		weather = scrape_weather()
+        # Getting summary of current weather. 
+        weather_main, weather_description, temp = scrape_weather()
 
-		# Getting JSON file with bike station data. 
-		json = scrape_bikes()
-		
-		# Connecting to DB
-		db.open()
+        # Getting JSON file with bike station data. 
+        json = scrape_bikes()
+        
+        # Connecting to DB
+        db.open()
 
-		# Looping through all objects in JSON file. 
-		for item in json: 
-			# Parsing data from each object. 
-			number, station, lat, lng, status, total_stands, free_stands, free_bikes, updated = parse_json(item)
-			# Inserting weather + bike data into DB. 
-			db.insert(number, station, lat, lng, status, total_stands, free_stands, free_bikes, weather, updated)
+        # Looping through all objects in JSON file. 
+        for item in json: 
+            # Parsing data from each object. 
+            number, station, lat, lng, status, total_stands, free_stands, free_bikes, updated = parse_json(item)
+            # Inserting weather + bike data into DB. 
+            db.insert(number, station, lat, lng, status, total_stands, free_stands, free_bikes, weather_main, weather_description, temp, updated)
 
-		# Disconnecting from DB
-		db.close()
+        # Disconnecting from DB
+        db.close()
 
-		# Sleep until next cycle. 
-		time.sleep(5 * 60)
+        # Sleep until next cycle. 
+        time.sleep(5 * 60)
 
 
-#--------------------------------------------------------------------------#		
+#--------------------------------------------------------------------------#        
 def scrape_weather():
-	'''Returns a summary of the current weather from the OpenWeather API'''
-	# API URI 
-	URI = 'http://api.openweathermap.org/data/2.5/weather'
-	# API Parameters 
-	params = {'q': 'Dublin', 'appid': 'e9b71166c5c433f0066ecbf407c8d9dc'}
+    '''Returns a summary of the current weather from the OpenWeather API'''
+    # API URI 
+    URI = 'http://api.openweathermap.org/data/2.5/weather'
+    # API Parameters 
+    params = {'q': 'Dublin', 'appid': 'e9b71166c5c433f0066ecbf407c8d9dc'}
 
-	# Loading Data 
-	try:
-		req = requests.get(URI, params=params)
-		data = json.loads(req.text)
+    # Loading Data 
+    try:
+        req = requests.get(URI, params=params)
+        data = json.loads(req.text)
 
-	except:
-		data = []
-		print(traceback.format_exc())
+    except:
+        data = []
+        print(traceback.format_exc())
 
-	# Returning Summary
-	return data['weather'][0]['description']
+    # Main Weather Group
+    weather_main = data['weather'][0]['main']
+    # Weather Description - probably won't be used in end, main group is better. 
+    weather_description = data['weather'][0]['description']
+    # Temperature
+    temp = data['main']['temp']
+
+    # Returning Summary
+    return weather_main, weather_description, temp
 
 
 
 #--------------------------------------------------------------------------#
 def scrape_bikes():
-	'''Returns a JSON file containing data relating to Dublin Bike stations'''
-	# API URI 
-	URI = 'https://api.jcdecaux.com/vls/v1/stations'
-	# API Parameters 
-	params = {'contract': 'Dublin', 'apiKey': 'e651cb028b0b9aa6511acb9dcee4a6e84a5a8b41'}
+    '''Returns a JSON file containing data relating to Dublin Bike stations'''
+    # API URI 
+    URI = 'https://api.jcdecaux.com/vls/v1/stations'
+    # API Parameters 
+    params = {'contract': 'Dublin', 'apiKey': 'e651cb028b0b9aa6511acb9dcee4a6e84a5a8b41'}
 
-	# Loading Data 
-	try:
-		req = requests.get(URI, params=params)
-		data = json.loads(req.text)
-	
-	except:
-		data = []
-		print(traceback.format_exc())
+    # Loading Data 
+    try:
+        req = requests.get(URI, params=params)
+        data = json.loads(req.text)
+    
+    except:
+        data = []
+        print(traceback.format_exc())
 
-	# Returning Data
-	return data
+    # Returning Data
+    return data
 
 
 #--------------------------------------------------------------------------#
 def parse_json(item):
-	'''Parses station data from JSON file.'''
-	# Station Number
-	number = item['number']
-	# Station Name
-	station = item['address']
-	# Latitude
-	lat = item['position']['lat']
-	# Longitude
-	lng = item['position']['lng']
-	# Longitude
-	status = item['status']
-	# Total Bike Stands
-	total_stands = item['bike_stands']
-	# Total Bike Stands Available
-	free_stands = item['available_bike_stands']
-	# Total Bikes Available
-	free_bikes = item['available_bikes']
-	# Time of update, stored in a datetime object 
-	updated = datetime.datetime.fromtimestamp(item['last_update'] / 1e3)
-	
-	# Returning parsed data
-	return number, station, lat, lng, status, total_stands, free_stands, free_bikes, updated
+    '''Parses station data from JSON file.'''
+    # Station Number
+    number = item['number']
+    # Station Name
+    station = item['address']
+    # Latitude
+    lat = item['position']['lat']
+    # Longitude
+    lng = item['position']['lng']
+    # Longitude
+    status = item['status']
+    # Total Bike Stands
+    total_stands = item['bike_stands']
+    # Total Bike Stands Available
+    free_stands = item['available_bike_stands']
+    # Total Bikes Available
+    free_bikes = item['available_bikes']
+    # Time of update, stored in a datetime object 
+    updated = datetime.datetime.fromtimestamp(item['last_update'] / 1e3)
+    
+    # Returning parsed data
+    return number, station, lat, lng, status, total_stands, free_stands, free_bikes, updated
 
 
 #--------------------------------------------------------------------------#

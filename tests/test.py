f75c87decd8d18bf546495341ca8695d42bf3999
Eoin Kenny <eoinmkenny@gmail.com>
2017-03-30 15:24:11 +0100

    Added Test for Live Weather

--- tests/test.py
+++ tests/test.py
@@ -1,14 +1,21 @@
-from nose.tools import eq_
+from nose.tools import eq_, ok_
 from flask_site.bike_db.BikeDB import *
+from scraper.__main__ import scrape_weather
 
 """ Tests for Project Functionality
     Testing framework used --> Nose """
 
 
 def test_database_connection():
-    """ Will Return True if RDS exists """
+    """ Will Test if Database Connection was Successful """
 
     eq_(rds_connection(), True, 'Failed to establish a connection to the RDS')
+
+
+def test_live_weather():
+    """ Will Test if Live Open Weather API Call was successful """
+
+    ok_(scrape_weather(), 'No Live Weather Update from Open Weather')
 
 
 # Insert more test functions here...
@@ -28,5 +35,3 @@ def rds_connection():
 
     except:
         return False
-
-rds_connection()
